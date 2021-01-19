8d40c54bb4a7aa02c848d65395e0da5c3aadc0d3
kjwallac <kristinajwallace@gmail.com>
2021-01-18 21:11:35 -0800

    get current moment, loop through timeblocks and assign css class based on timing (past, present, future), display appointment text from local storage to description textareas

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-$(document).ready(function() {
-//schedule an event
-$(".saveBtn").on('click', function() {
-    console.log('this button works at least');
+$(document).ready(function () {
+  //schedule an event
+  $(".saveBtn").on("click", function () {
     //get the text
     const hour = $(this).siblings(0).text();
     const desc = $(this).parent().children().eq(1).val();
+    const timeBlockID = $(this).parent().attr("id");
     //set the item in localStorage
-    localStorage.setItem(hour, desc);
-    console.log({hour, desc});
+    localStorage.setItem(timeBlockID, desc);
+  });
+
+  //get current moment
+  const currentHour = moment().hours();
+  //loop through timeblocks
+  $(".time-block").each((index, timeBlock) => {
+    const hour = parseInt(timeBlock.id.split("-")[1]);
+    //add class to each timeblock based on timing
+    if (hour < currentHour) {
+      $(timeBlock).addClass("past");
+    } else if (hour > currentHour) {
+      $(timeBlock).addClass("future");
+    } else {
+      $(timeBlock).addClass("present");
+    }
+
+    //display appointment text from local storage
+    const appointment = localStorage.getItem(timeBlock.id);
+    const textArea = $(timeBlock).children().eq(1);
+    textArea.val(appointment);
+  });
+
+  //show current date
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
-
-});
-
-
-
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
